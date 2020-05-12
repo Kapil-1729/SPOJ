c5a5551e65cd6062687c9c013c8b56718b9b0dc2
Kapil-1729 <65090973+Kapil-1729@users.noreply.github.com>
2020-05-12 15:30:06 +0530

    Create CAM5.cpp

---
+++ CAM5.cpp
@@ -0,0 +1,57 @@
+#include<bits/stdc++.h>
+using namespace std;
+void dfs(int s, vector<int> g[], bool vis[])
+{
+    
+    vis[s]=true;
+    for(int i=0;i<g[s].size();i++)
+    {
+        if(vis[g[s][i]]!=true)
+        {
+            
+            dfs(g[s][i],g,vis);
+        }
+    }
+
+}
+
+// { Driver Code Starts.
+
+int main()
+{
+    int T;
+    cin>>T;
+    while(T--)
+    {
+
+        int N, E;
+        int result=0;
+        cin>>N>>E;
+        
+        vector<int> g[N];
+        bool vis[N];
+        
+        memset(vis, false, sizeof(vis));
+        
+        for(int i=0;i<E;i++)
+        {
+            int u,v;
+            cin>>u>>v;
+            g[u].push_back(v);
+            g[v].push_back(u);
+        }
+		
+		for (int i=0;i<N;i++)
+		{
+			if(vis[i]!=true)
+			{
+				result+=1;
+				dfs(i,g,vis);
+			}
+		}
+        
+        
+    cout<<result<<endl;
+
+    }
+} 
